fe7a2669ab929fdb27ec04d6a3d091c602b72aba
otakustay <otakustay@gmail.com>
2017-09-03 18:41:54 +0800

    Beautify comment style

--- demo/CommentWidget.js
+++ demo/CommentWidget.js
@@ -1,9 +1,17 @@
 import CommentInput from './CommentInput';
+import CommentIcon from './CommentIcon.svg';
 import './CommentWidget.css';
+
+const Comment = ({content}) => (
+    <div className="comment">
+        <CommentIcon className="comment-icon" />
+        <div className="comment-content">{content}</div>
+    </div>
+);
 
 const CommentWidget = ({comments, writing, onSave}) => (
     <div className="comment-widget">
-        {comments.map((content, i) => <pre key={i} className="comment">{content}</pre>)}
+        {comments.map((content, i) => <Comment key={i} content={content} />)}
         {writing && <CommentInput onSave={onSave} />}
     </div>
 );
