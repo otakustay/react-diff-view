28384e4adbd3648159308420f44b7afa5b1d720c
otakustay <otakustay@gmail.com>
2017-12-28 19:11:15 +0800

    Fix a bug where code expanding range includes insert or delete change results incorrect hunks

--- src/utils.js
+++ src/utils.js
@@ -94,12 +94,16 @@ const createIsInHunkFunction = (startProperty, linesProperty) => (hunk, lineNumb
     return lineNumber >= start && lineNumber < end;
 };
 
+const isOldLineNumberInHunk = createIsInHunkFunction('oldStart', 'oldLines');
+
 const createIsBetweenHunksFunction = (startProperty, linesProperty) => (previousHunk, nextHunk, lineNumber) => {
     const start = previousHunk[startProperty] + previousHunk[linesProperty];
     const end = nextHunk[startProperty];
 
     return lineNumber >= start && lineNumber < end;
 };
+
+const isOldLineNumberBetweenHunks = createIsBetweenHunksFunction('oldStart', 'oldLines');
 
 const createFindContainerHunkFunction = side => {
     const startProperty = side + 'Start';
@@ -109,7 +113,32 @@ const createFindContainerHunkFunction = side => {
     return (hunks, lineNumber) => hunks.find(hunk => isInHunk(hunk, lineNumber));
 };
 
-const findContainerHunkByOldLineNumber = createFindContainerHunkFunction('old');
+const findCorrespondingValidHunkIndex = (hunks, oldLineNumber) => {
+    if (!hunks.length) {
+        return -1;
+    }
+
+    const firstHunk = first(hunks);
+    if (oldLineNumber < firstHunk.oldStart || isOldLineNumberInHunk(firstHunk, oldLineNumber)) {
+        return 0;
+    }
+
+    for (let i = 1; i < hunks.length; i++) {
+        const currentHunk = hunks[i];
+
+        if (isOldLineNumberInHunk(currentHunk)) {
+            return i;
+        }
+
+        const previousHunk = hunks[i - 1];
+
+        if (isOldLineNumberBetweenHunks(previousHunk, currentHunk, oldLineNumber)) {
+            return i;
+        }
+    }
+
+    return -1;
+};
 
 const createFindChangeByLineNumberFunction = side => {
     const computeLineNumber = side === 'old' ? computeOldLineNumber : computeNewLineNumber;
@@ -302,45 +331,92 @@ export const insertHunk = (hunks, insertion) => {
     return hunksWithInsertion.reduce(appendOrMergeHunk, []);
 };
 
-const adjustStartToNormalChange = (hunks, start) => {
-    const hunk = findContainerHunkByOldLineNumber(hunks, start);
-
-    if (!hunk) {
-        return start;
-    }
-
-    const {changes} = hunk;
+const findNearestNormalChangeIndex = ({changes}, start) => {
     const index = changes.findIndex(change => computeOldLineNumber(change) === start);
 
     for (let i = index; i < changes.length; i++) {
         const change = changes[i];
 
         if (change.isNormal) {
-            return computeOldLineNumber(change);
+            return i;
         }
     }
-
-    throw new Error(`There is no nearby normal change after line ${start} in hunks`);
 };
 
-export const expandFromRawCode = (hunks, rawCodeOrLines, start, end) => {
-    // It is not possible to  slice from a normal change since there is no corresponding new line number.
-    //
-    // In this case, we need to find the nearest subsequent normal change and start from there.
-    //
-    // The issue does not apply to `end` because we need only a `newStart` property in hunk object.
-    const adjustedStart = adjustStartToNormalChange(hunks, start);
+const splitRangeToValidOnes = (hunks, start, end) => {
+    const correspondingHunkIndex = findCorrespondingValidHunkIndex(hunks, start);
 
+    // `start` is after all hunks, we believe all left lines are normal.
+    if (correspondingHunkIndex === -1) {
+        return [[start, end]];
+    }
+
+    const correspondingHunk = hunks[correspondingHunkIndex];
+
+    // If `start` points to a line before this hunk, we collect all heading normal changes
+    if (start < correspondingHunk.oldStart) {
+        const headingChangesCount = correspondingHunk.changes.findIndex(change => !change.isNormal);
+        const validEnd = correspondingHunk.oldStart + Math.max(headingChangesCount, 0);
+
+        if (validEnd >= end) {
+            return [[start, end]];
+        }
+
+        return [
+            [start, validEnd],
+            ...splitRangeToValidOnes(hunks, validEnd + 1, end)
+        ];
+    }
+
+    // Now the `correspondingHunk` must be a hunk containing `start`
+    const {changes} = correspondingHunk;
+    const nearestNormalChangeIndex = findNearestNormalChangeIndex(correspondingHunk, start);
+    const validStartChange = changes[nearestNormalChangeIndex];
+    // Iterate to `end`, if `end` falls out of hunk, we can split it to 2 ranges
+    const adjacentChangesCount = changes.slice(nearestNormalChangeIndex + 1).findIndex(change => !change.isNormal);
+    const validEnd = computeOldLineNumber(validStartChange) + Math.max(adjacentChangesCount, 0);
+
+    if (validEnd >= end) {
+        return [[start, end]];
+    }
+
+    return [
+        [start, validEnd],
+        ...splitRangeToValidOnes(hunks, validEnd + 1, end)
+    ];
+};
+
+const expandCodeByValidRange = (hunks, rawCodeOrLines, [start, end]) => {
     // Note `end` is not inclusive, this is the same as `Array.prototype.slice` method
     const linesOfCode = typeof rawCodeOrLines === 'string' ? rawCodeOrLines.split('\n') : rawCodeOrLines;
-    const slicedLines = linesOfCode.slice(Math.max(adjustedStart, 1) - 1, end - 1);
+    const slicedLines = linesOfCode.slice(Math.max(start, 1) - 1, end - 1);
 
     if (!slicedLines.length) {
         return hunks;
     }
 
-    const slicedHunk = textLinesToHunk(slicedLines, adjustedStart, getCorrespondingNewLineNumber(hunks, adjustedStart));
+    const slicedHunk = textLinesToHunk(slicedLines, start, getCorrespondingNewLineNumber(hunks, start));
     return insertHunk(hunks, slicedHunk);
+};
+
+export const expandFromRawCode = (hunks, rawCodeOrLines, start, end) => {
+    // It is possible to have some insert or delete changes between `start` and `end`,
+    // in order to be 100% safe, we need to split the range to one or more ranges which contains only normal changes.
+    //
+    // For each `start` line number, we can either:
+    //
+    // 1. Find a change and adjust to a nearest normal one.
+    // 2. Find no corresponding change so it must be a collapsed normal change.
+    //
+    // For both cases we can have a starting normal change, then we iterate over its subsequent changes
+    // (line numbers with no corresponding change is considered a normal one)
+    // until an insert or delete is encountered, this is a **valid range**.
+    //
+    // After one valid range is resolved, discard all line numbers related to delete changes, the next normal change
+    // is the start of next valid range.
+    const validRanges = splitRangeToValidOnes(hunks, start, end);
+
+    return validRanges.reduce((hunks, range) => expandCodeByValidRange(hunks, rawCodeOrLines, range), hunks);
 };
 
 export const getCollapsedLinesCountBetween = (previousHunk, nextHunk) => {
