c8f1872fca78bb3776192d30d393c57e5c25dea7
otakustay <otakustay@gmail.com>
2018-10-15 20:02:39 +0800

    Allow unidiff as input of parse function
    This is a mirror commit of 9800d6bd33b48afd61a0fcc7bd574e7e4d948ece

--- src/utils/parse.js
+++ src/utils/parse.js
@@ -43,8 +43,29 @@ const mapFile = (file, options) => {
     return {...file, hunks};
 };
 
+const normalizeDiffText = text => {
+    if (text.indexOf('diff --git') === 0) {
+        return text;
+    }
+
+    const indexOfFirstLineBreak = text.indexOf('\n');
+    const indexOfSecondLineBreak = text.indexOf('\n', indexOfFirstLineBreak + 1);
+    const firstLine = text.slice(0, indexOfFirstLineBreak);
+    const secondLine = text.slice(indexOfFirstLineBreak + 1, indexOfSecondLineBreak);
+    const oldPath = firstLine.slice(4);
+    const newPath = secondLine.slice(4);
+    const segments = [
+        `diff --git ${oldPath} ${newPath}`,
+        'index 1111111..2222222 100644',
+        text
+    ];
+
+    return segments.join('\n');
+};
+
 export const parseDiff = (text, options = {}) => {
-    const files = parser.parse(text);
+    const diffText = normalizeDiffText(text);
+    const files = parser.parse(diffText);
 
     return files.map(file => mapFile(file, options));
 };
