1d0a377b753f62c190a8e702f9b499668419919d
Cong Zhang <dancerphil1994@gmail.com>
2018-10-25 16:32:06 +0800

    first snapshot test

--- README.md
+++ README.md
@@ -99,7 +99,7 @@ The `Diff` named export is a component to render a diff, a simplest case to rend
 import {parseDiff, Diff, Hunk} from 'react-diff-view';
 
 const App = ({diffText}) => {
-    const {files} = parseDiff(diffText);
+    const files = parseDiff(diffText);
 
     const renderFile = ({oldRevision, newRevision, type, hunks}) => (
         <Diff key={oldRevision + '-' + newRevision} viewType="split" diffType={type} hunks={hunks}>
---
+++ src/__test__/index.test.jsx
@@ -0,0 +1,36 @@
+import renderer from 'react-test-renderer';
+import {parseDiff, Diff, Hunk} from '../index';
+
+const diffTextExample = `
+diff --git a/src/__test__/index.test.jsx b/src/__test__/index.test.jsx
+index 643c2f0..7883597 100644
+--- a/src/__test__/index.test.jsx
++++ b/src/__test__/index.test.jsx
+@@ -21,3 +21,3 @@ describe('basic test', () => {
+     test('App renders correctly', () => {
+-        expect(renderer.create(<App diffText={'deff'} />).toJSON()).toMatchSnapshot();
++        expect(renderer.create(<App diffText={'diff'} />).toJSON()).toMatchSnapshot();
+     });
+`
+
+const App = ({diffText}) => {
+    const files = parseDiff(diffText);
+
+    const renderFile = ({oldRevision, newRevision, type, hunks}) => (
+        <Diff key={oldRevision + '-' + newRevision} viewType="split" diffType={type} hunks={hunks}>
+            {hunks => hunks.map(hunk => <Hunk key={hunk.content} hunk={hunk} />)}
+        </Diff>
+    );
+
+    return (
+        <div>
+            {files.map(renderFile)}
+        </div>
+    );
+};
+
+describe('basic test', () => {
+    test('App renders correctly', () => {
+        expect(renderer.create(<App diffText={'asdf'} />).toJSON()).toMatchSnapshot();
+    });
+});
