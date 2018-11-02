d208b847ae732db3413526a471cdba53c955058c
Cong Zhang <dancerphil1994@gmail.com>
2018-11-02 15:40:54 +0800

    test withSourceExpansion

--- src/hocs/__test__/withSourceExpansion.test.js
+++ src/hocs/__test__/withSourceExpansion.test.js
@@ -1,5 +1,7 @@
 import renderer from 'react-test-renderer';
 import {withSourceExpansion} from '..';
+import {Diff, Hunk} from '../..';
+import {basic, basicHunk, multipleHunk} from '../../__test__/cases';
 
 const ComponentIn = () => <div />;
 
@@ -15,5 +17,56 @@ describe('withSourceExpansion', () => {
     test('render component', () => {
         const ComponentOut = withSourceExpansion()(ComponentIn);
         expect(renderer.create(<ComponentOut />).toJSON()).toMatchSnapshot();
+    });
+});
+
+const UnfoldCollapsed = ({previousHunk, currentHunk, onClick}) => {
+    const start = previousHunk ? previousHunk.oldStart + previousHunk.oldLines : 1;
+    const end = currentHunk.oldStart - 1;
+
+    /* eslint-disable react/jsx-no-bind */
+    return (
+        <div onClick={() => onClick(start, end)}>
+            Click to expand
+        </div>
+    );
+    /* eslint-enable react/jsx-no-bind */
+};
+
+const renderHunk = onExpandRange => (children, hunk) => {
+    const previousElement = children[children.length - 1];
+    const decorationElement = (
+        <UnfoldCollapsed
+            key={'decoration-' + hunk.content}
+            previousHunk={previousElement && previousElement.props.hunk}
+            currentHunk={hunk}
+            onClick={onExpandRange}
+        />
+    );
+    children.push(decorationElement);
+
+    const hunkElement = (
+        <Hunk
+            key={'hunk-' + hunk.content}
+            hunk={hunk}
+        />
+    );
+    children.push(hunkElement);
+
+    return children;
+};
+
+const DiffView = ({hunks, onExpandRange}) => (
+    <Diff hunks={hunks} diffType="modify" viewType="split">
+        {hunks => hunks.reduce(renderHunk(onExpandRange), [])}
+    </Diff>
+);
+
+describe('withSourceExpansion document usage', () => {
+    test('renders correctly', () => {
+        const EnhancedDiffView = withSourceExpansion()(DiffView);
+        expect(renderer.create(
+            <EnhancedDiffView oldSource={basic} hunks={[basicHunk]} />
+        ).toJSON()).toMatchSnapshot();
     });
 });
