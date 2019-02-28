6aa5399c52392e19f7f8fbe4af17b374b4339862
otakustay <otakustay@gmail.com>
2019-02-28 16:23:42 +0800

    Move custom hook from inside SplitChange to top scope
    It seems really weird to "define a hook" inside a component function.
    
    Also in this commit I remove the `mergeCallbacks` utility function and implement it directly inside `useCallbackOnSide` hook, which leads a little improvement on code readability in my point of view.

--- src/utils/callbacks.js
+++ src/utils/callbacks.js
@@ -1,27 +1,4 @@
-import {mapValues} from 'lodash';
 import arrayShallowEquals from 'shallow-equal/arrays';
-
-const composeCallback = (customCallback, ownCallback) => e => {
-    ownCallback(e);
-    customCallback(); // `customCallback` is already bound with `arg`
-};
-
-export const mergeCallbacks = (own, custom, arg) => {
-    const boundCustomCallbacks = mapValues(custom, fn => () => fn(arg));
-
-    return Object.entries(own).reduce(
-        (callbacks, [name, ownCallback]) => {
-            const customCallback = callbacks[name];
-            const callback = customCallback ? composeCallback(customCallback, ownCallback) : ownCallback;
-
-            return {
-                ...callbacks,
-                [name]: callback,
-            };
-        },
-        boundCustomCallbacks
-    );
-};
 
 // TODO: Delete it after all class components are converted to hooks based.
 export const createSelector = (select, inputEquals = arrayShallowEquals) => {
